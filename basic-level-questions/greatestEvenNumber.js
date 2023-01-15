f915ed2ffac824312066514214402c0c2524f99b
Rahul Behera <rahulbehera2002@gmail.com>
2023-01-15 17:06:42 +0530

    test: add tests for greatestEvenNumber file

--- basic-level-questions/greatestEvenNumber.js
+++ basic-level-questions/greatestEvenNumber.js
@@ -1,4 +1,14 @@
+const checkForNonIntegerInArray = (arr) => {
+    arr.forEach((ele) => {
+        if(typeof(ele) !== "number")
+            return true;
+    })
+    return false;
+}
+
 const greatestEvenNumber = (arr) => {
+    if(checkForNonIntegerInArray(arr))
+        return -1;
     arr = arr.filter(it => it % 2 === 0);
     if(arr.length === 0)
         return -1;
@@ -6,11 +16,15 @@ const greatestEvenNumber = (arr) => {
 }
 
 function largestEvenNumber(arr) {
+    if(checkForNonIntegerInArray(arr))
+        return -1;
     arr = arr.filter(function (it) { return it % 2 === 0 });
     if(arr.length === 0)
         return -1;
     return Math.max(...arr);
 }
 
-console.log(greatestEvenNumber([1, 3, 5, 7]));
-console.log(largestEvenNumber([1, 2, 3, 4, 5]));
+module.exports = { greatestEvenNumber, largestEvenNumber };
+
+// console.log(greatestEvenNumber([1, 3, 5, 7]));
+// console.log(largestEvenNumber([1, 2, 3, 4, 5]));
---
+++ basic-level-questions/greatestEvenNumber.test.js
@@ -0,0 +1,49 @@
+const { greatestEvenNumber, largestEvenNumber } = require("./greatestEvenNumber");
+
+test('when given a array of number then return largest even number', 
+    () => { 
+        expect(greatestEvenNumber([1, 2, 3, 4, 5])).toBe(4);
+    }
+);
+
+test('when given a array of odd number then return -1', 
+    () => { 
+        expect(greatestEvenNumber([1, 3, 5, 7])).toBe(-1);
+    }
+);
+
+test('when given a empty array then return -1', 
+    () => { 
+        expect(greatestEvenNumber([])).toBe(-1);
+    }
+);
+
+test('when given a array of number then return largest even number', 
+    () => { 
+        expect(greatestEvenNumber(["1", 3, 5, 7])).toBe(-1);
+    }
+);
+
+test('when given a array of number then return largest even number', 
+    () => { 
+        expect(largestEvenNumber([1, 2, 3, 4, 5])).toBe(4);
+    }
+);
+
+test('when given a array of odd number then return -1', 
+    () => { 
+        expect(largestEvenNumber([1, 3, 5, 7])).toBe(-1);
+    }
+);
+
+test('when given a empty array then return -1', 
+    () => { 
+        expect(largestEvenNumber([])).toBe(-1);
+    }
+);
+
+test('when given a array of number then return largest even number', 
+    () => { 
+        expect(largestEvenNumber(["1", 3, 5, 7])).toBe(-1);
+    }
+);
