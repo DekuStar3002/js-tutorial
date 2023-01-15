5eda85796557719bbc584a57f6e249e03d4f2cf0
Rahul Behera <rahulbehera2002@gmail.com>
2023-01-15 16:26:29 +0530

    test: add tests for findLongestString file

--- basic-level-questions/findLongestString.js
+++ basic-level-questions/findLongestString.js
@@ -1,6 +1,10 @@
 const findLongestString = (strArray) => {
+    if(strArray.length === 0)
+        return "Expected atleast 1 element";
     let longestStr = "";
     for(let i = 0; i < strArray.length; ++i) {
+        if(typeof(strArray[i]) !== "string")
+            return "error: received element was not a string";
         if(longestStr.length < strArray[i].length)
             longestStr = strArray[i];
     }
@@ -8,13 +12,19 @@ const findLongestString = (strArray) => {
 }
 
 function searchLongestString(strArray) {
+    if(strArray.length === 0)
+        return "Expected atleast 1 element";
     let longestStr = "";
     for(let i = 0; i < strArray.length; ++i) {
+        if(typeof(strArray[i]) !== "string")
+            return "error: received element was not a string";
         if(longestStr.length < strArray[i].length)
             longestStr = strArray[i];
     }
     return longestStr;
 }
 
-console.log(findLongestString(["we", "love", "code", "academy"]));
-console.log(searchLongestString(["we", "love", "code", "academy"]));
+module.exports = { findLongestString, searchLongestString };
+
+// console.log(findLongestString(["we", "love", "code", "academy"]));
+// console.log(searchLongestString(["we", "love", "code", "academy"]));
---
+++ basic-level-questions/findLongestString.test.js
@@ -0,0 +1,39 @@
+const { findLongestString, searchLongestString } = require("./findLongestString");
+
+test('when given an array of strings then return longest string', 
+    () => { 
+        expect(findLongestString(["we", "love", "code", "academy"])).toBe("academy");
+    }
+);
+
+test('when given an empty array return error message', 
+    () => { 
+        expect(findLongestString([])).toBe("Expected atleast 1 element");
+    }
+);
+
+test('when given an array of non strings then return longest string', 
+    () => { 
+        expect(findLongestString([1, 1.2, "code", "academy"])).toBe("error: received element was not a string");
+    }
+);
+
+test('when given an array of strings then return longest string', 
+    () => { 
+        expect(searchLongestString(["we", "love", "code", "academy"])).toBe("academy");
+    }
+);
+
+test('when given an empty array return error message', 
+    () => { 
+        expect(searchLongestString([])).toBe("Expected atleast 1 element");
+    }
+);
+
+test('when given an array of non strings then return longest string', 
+    () => { 
+        expect(searchLongestString([1, 1.2, "code", "academy"])).toBe("error: received element was not a string");
+    }
+);
+
+
