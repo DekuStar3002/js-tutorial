72aae4f2eb874a6bd7483883ea5577b97673bee6
Rahul Behera <rahulbehera2002@gmail.com>
2023-01-15 13:52:47 +0530

    test: add tests for allDigitsSame file

--- basic-level-questions/allDigitsSame.js
+++ basic-level-questions/allDigitsSame.js
@@ -1,20 +1,37 @@
+const numberCheckArray = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
 const isAllDigitsSame = number => {
     const numberString = number.toString();
+    if(numberString.length === 0)
+        return false;
     const firstChar = numberString.charAt(0);
-    for(let head = 0, tail = numberString.length - 1; head < tail; ++head, --tail)
-        if(firstChar !== numberString.charAt(head) || firstChar !== numberString.charAt(tail))
+    for(let head = 0, tail = numberString.length - 1; head < tail; ++head, --tail){
+        if(numberCheckArray.includes(numberString.charAt(head)) && numberCheckArray.includes(numberString.charAt(tail))) {
+            if(firstChar !== numberString.charAt(head) || firstChar !== numberString.charAt(tail))
+                return false;
+        } else {
             return false;
-    return true
+        }
+    }
+    return true;
 }
 
 function allDigitsSame(number) {
     const numberString = number.toString();
+    if(numberString.length === 0)
+        return false;
     const firstChar = numberString.charAt(0);
-    for(let head = 0, tail = numberString.length - 1; head < tail; ++head, --tail)
-        if(firstChar !== numberString.charAt(head) || firstChar !== numberString.charAt(tail))
+    for(let head = 0, tail = numberString.length - 1; head < tail; ++head, --tail){
+        if(numberCheckArray.includes(numberString.charAt(head)) && numberCheckArray.includes(numberString.charAt(tail))) {
+            if(firstChar !== numberString.charAt(head) || firstChar !== numberString.charAt(tail))
+                return false;
+        } else {
             return false;
-    return true
+        }
+    }
+    return true;
 }
 
-console.log(`Is all digit same?: ${isAllDigitsSame(22)}`);
-console.log(`Is all digit same?: ${allDigitsSame(23)}`);
+// console.log(`Is all digit same?: ${isAllDigitsSame(22)}`);
+// console.log(`Is all digit same?: ${allDigitsSame(23)}`);
+
+module.exports = { isAllDigitsSame, allDigitsSame };
